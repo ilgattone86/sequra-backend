f8222bb849e165499992b7ffed156f7bcf1a5d4a
ilgattone86 <mgattellicmc@gmail.com>
2025-05-19 17:42:41 +0200

    seed the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'csv'
+
+def seed_merchants
+  merchants = []
+  ::CSV.foreach('db/seeds/merchants.csv', headers: true, col_sep: ';') do |row|
+    merchants << { email: row['email'],
+                   reference: row['reference'],
+                   live_on: Date.parse(row['live_on']),
+                   minimum_monthly_fee: row['minimum_monthly_fee'].to_f.round(2),
+                   disbursement_frequency: row['disbursement_frequency'] == 'DAILY' ? 0 : 1 }
+  end
+  ::Merchant.insert_all!(merchants)
+end
+
+def seed_orders
+  merchants_by_reference = ::Merchant.all.index_by(&:reference)
+
+  orders = []
+  ::CSV.foreach('db/seeds/orders.csv', headers: true, col_sep: ';') do |row|
+    merchant = merchants_by_reference[row['merchant_reference']]
+    orders << { merchant_id: merchant.id,
+                amount: row['amount'].to_f.round(2),
+                order_received_at: Date.parse(row['created_at']) }
+  end
+  ::Order.insert_all!(orders)
+end
+
+# The order here is important
+seed_merchants
+seed_orders
